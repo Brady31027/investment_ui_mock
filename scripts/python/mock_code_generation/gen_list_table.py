bd7a2332f7606408dc7ced71aada70beea2d4752
Brady Cheng <brady3127@gmail.com>
2021-01-25 11:12:52 +0800

    refine list UI

--- scripts/python/mock_code_generation/gen_list_table.py
+++ scripts/python/mock_code_generation/gen_list_table.py
@@ -1,7 +1,7 @@
 import json
 
-with open('../get_stock_list/dist/sp500_list.json') as f:
-  entries = json.load(f)['sp500_list']
+with open('../get_stock_list/dist/nasdaq_list.json') as f:
+  entries = json.load(f)['nasdaq_list']
 
 with open('dow_html.txt', 'w') as out_file:
   out_file.write('<table class="table">\n')
@@ -9,10 +9,7 @@ with open('dow_html.txt', 'w') as out_file:
   for entry in entries:
     out_file.write('<tr>\n')
     out_file.write('<td>')
-    out_file.write(entry['symbol'])
-    out_file.write('</td>\n')
-    out_file.write('<td>')
-    out_file.write(entry['title'])
+    out_file.write(entry['title']+' ('+ entry['symbol']+')')
     out_file.write('</td>\n')
     out_file.write('<td>')
     out_file.write('<button type="button" class="btn btn-info">More</button>')
